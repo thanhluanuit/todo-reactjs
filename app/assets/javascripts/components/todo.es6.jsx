e2a7fb80e2289b54f9503c80018cead78f10f169
Thanh Luan <thanhluan.uit@gmail.com>
2017-07-19 16:06:55 +0700

    Add event to handle update Status of todo list

--- app/assets/javascripts/components/todo.es6.jsx
+++ app/assets/javascripts/components/todo.es6.jsx
@@ -1,10 +1,33 @@
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {status: props.todo.status};
+    this.handleChangeStatus = this.handleChangeStatus.bind(this);
+  }
+
+  handleChangeStatus(event) {
+    this.setState({status: event.target.checked});
+    this.updateStatusToServer(event);
+  }
+
+  updateStatusToServer(event) {
+    const id = event.target.value;
+
+    $.ajax({
+      type: 'PUT',
+      url: `/todos/${id}`,
+      data: {
+        todo: {status: event.target.checked}
+      }
+    })
+  }
+
   render() {
     const todo = this.props.todo;
 
     return(
       <div className='todo'>
-        <input type='checkbox' checked={todo.status}/>
+        <input type='checkbox' defaultValue={todo.id} checked={this.state.status} onChange={this.handleChangeStatus} />
         <span>{todo.content}</span>
       </div>
     )
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,19 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update!(todo_params)
+
+    respond_to do |format|
+      format.json { render json: {status: todo.status} }
+    end
+  end
+
+  protected
+
+  def todo_params
+    params.require(:todo).permit(:status, :content)
+  end
 end
